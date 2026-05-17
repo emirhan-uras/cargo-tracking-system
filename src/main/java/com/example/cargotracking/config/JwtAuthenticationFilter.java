e9f708c4f1df825c1a0eff4d70fdcd7c762d7f15
emirhan-uras <emirhanuras35@gmail.com>
2026-05-17 21:04:19 +0300

    feat: configure Spring Security filter chain and password encoder

--- src/main/java/com/example/cargotracking/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/cargotracking/config/JwtAuthenticationFilter.java
@@ -57,7 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        
+
         filterChain.doFilter(request, response);
     }
 }
